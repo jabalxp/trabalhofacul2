222ad563d683ecd0a8a2bd3b50698b9bcc7b87b0
Rafael Adriano <144788933+jabalxp@users.noreply.github.com>
2026-03-27 19:49:00 -0300

    Add files via upload

--- valida_login.php
+++ valida_login.php
@@ -1,13 +1,13 @@
 <?php
 // Dados de acesso "Chumbados"
-$userMaster = "admin@blog.com";
-$passMaster = "123456";
+$userMaster = "rafael.adriano2801@gmail.com";
+$passMaster = "esqueciasenha";
 // Recebendo via POST
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 if ($email == $userMaster && $senha == $passMaster) {
     // Sucesso: Vai para o Dashboard
-    header("Location: index.php");
+    header("Location: dashboard.php");
 } else {
     // Erro: Volta para o Login com um sinalizador
     header("Location: index.php?erro=1");
